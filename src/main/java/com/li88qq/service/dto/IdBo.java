0656203c4e43fa9277cb3dd390613da3b9342fcf
li88qq <573258011@qq.com>
2021-07-11 11:33:19 +0800

    初始化项目

---
+++ src/main/java/com/li88qq/service/dto/IdBo.java
@@ -0,0 +1,17 @@
+package com.li88qq.service.dto;
+
+/**
+ * id请求参数
+ */
+public class IdBo {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
