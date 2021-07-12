1e83f6c9698d10aeef18fe432b5b9d6ac1297f77
li88qq <573258011@qq.com>
2021-07-12 23:20:20 +0800

    增加Ids公共bo.

---
+++ src/main/java/com/li88qq/service/dto/IdsBo.java
@@ -0,0 +1,17 @@
+package com.li88qq.service.dto;
+
+/**
+ * id列表bo
+ */
+public class IdsBo {
+
+    private String ids;
+
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+}
