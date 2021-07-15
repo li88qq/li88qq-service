c61f0376420404c79253f32ee26d8301f70c0a8d
li88qq <573258011@qq.com>
2021-07-15 23:06:40 +0800

    调整启动类注解扫描

--- src/main/java/com/li88qq/service/Application.java
+++ src/main/java/com/li88qq/service/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "org.fastquery.service")
+@ComponentScan(basePackages = {"org.fastquery.service", "com.li88qq.service"})
 @SpringBootApplication
 public class Application {
 
