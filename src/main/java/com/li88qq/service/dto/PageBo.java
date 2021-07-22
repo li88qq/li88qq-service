8a054d6454f65e9e32c8f48bd9823b9ba20a2017
li88qq <573258011@qq.com>
2021-07-22 23:23:16 +0800

    文章分页查询.

---
+++ src/main/java/com/li88qq/service/dto/PageBo.java
@@ -0,0 +1,23 @@
+package com.li88qq.service.dto;
+
+public class PageBo {
+
+    private Integer page;
+    private Integer size;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
