126848a975b95d53263a1f64481509416c492b95
li88qq <573258011@qq.com>
2021-08-01 18:03:17 +0800

    修改密码增加注解校验.

--- src/main/java/com/li88qq/service/request/my/UpdatePasswordBo.java
+++ src/main/java/com/li88qq/service/request/my/UpdatePasswordBo.java
@@ -1,8 +1,16 @@
 package com.li88qq.service.request.my;
 
+import com.li88qq.service.constant.annitions.Password;
+
+import javax.validation.constraints.NotBlank;
+
 public class UpdatePasswordBo {
 
+    @Password
+    @NotBlank(message = "请输入旧密码")
     private String oldPassword;
+    @Password
+    @NotBlank(message = "请输入密码")
     private String password;
 
     public String getOldPassword() {
