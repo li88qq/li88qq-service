5e325744d3592bfc65f90312dd9f4bb5bb9b2429
li88qq <573258011@qq.com>
2021-08-12 23:41:19 +0800

    操作日记注解重命名.

--- src/main/java/com/li88qq/service/constant/annitions/AcLog.java
+++ src/main/java/com/li88qq/service/constant/annitions/AcLog.java
@@ -12,11 +12,11 @@ import java.lang.annotation.Target;
  * 操作记录
  *
  * @author li88qq
- * @version 1.0 2021/8/11 23:24
+ * @version 1.0 2021/8/12 22:34
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ActionLog {
+public @interface AcLog {
 
     SaveStrategy strategy() default SaveStrategy.SUCCESS;//保存策略
 
@@ -24,6 +24,5 @@ public @interface ActionLog {
 
     String title();//标题
 
-    String detail();//详情
-
+    String detail() default "";//详情,格式:参数名|参数,参数;参数名称; 如:bo|id,name;id;
 }
