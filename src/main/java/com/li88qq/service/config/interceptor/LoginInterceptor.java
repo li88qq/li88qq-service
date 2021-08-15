f3c4ddf535c73665ef86a1fd4aef44f48ee8801f
li88qq <573258011@qq.com>
2021-08-15 18:29:30 +0800

    登录拦截器增加体验账号拦截处理.

--- src/main/java/com/li88qq/service/config/interceptor/LoginInterceptor.java
+++ src/main/java/com/li88qq/service/config/interceptor/LoginInterceptor.java
@@ -5,7 +5,6 @@ import com.li88qq.service.constant.enumeration.ResponseState;
 import com.li88qq.service.dto.BaseResponse;
 import com.li88qq.service.utils.ResponseUtil;
 import com.li88qq.service.utils.SessionUtil;
-import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,16 +20,28 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = SessionUtil.getSession(false);
-        if (session == null) {
-            errorResponse(response);
+        if (session == null || SessionUtil.getUid() == null) {
+            BaseResponse error = ResponseUtil.response(ResponseState.NO_LOGIN);
+            errorResponse(response, error);
             return false;
         }
+
+        String method = request.getMethod();
+        if (method.equals("POST")) {
+            boolean visitor = SessionUtil.getUser().isVisitor();
+            String path = request.getRequestURI();
+            if (visitor && !path.equals("/logout")) {
+                BaseResponse error = ResponseUtil.error("体验账号不支持该操作");
+                errorResponse(response, error);
+                return false;
+            }
+        }
+
         return true;
     }
 
-    private void errorResponse(HttpServletResponse response) throws IOException {
+    private void errorResponse(HttpServletResponse response, BaseResponse error) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
-        BaseResponse error = ResponseUtil.response(ResponseState.NO_LOGIN);
         response.getWriter().write(JSON.toJSONString(error));
         response.flushBuffer();
     }
