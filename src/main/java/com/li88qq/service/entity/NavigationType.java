cc5871f870079c7fb8658e4f455f1b34483e27f3
li88qq <573258011@qq.com>
2021-08-20 23:39:19 +0800

    增加导航和导航分类实体.

---
+++ src/main/java/com/li88qq/service/entity/NavigationType.java
@@ -0,0 +1,54 @@
+package com.li88qq.service.entity;
+
+import com.li88qq.service.utils.DateUtil;
+import org.fastquery.core.Id;
+import org.fastquery.core.Table;
+
+/**
+ * 导航类型
+ *
+ * @author li88qq
+ * @version 1.0 2021/8/20 23:36
+ */
+@Table("NavigationType")
+public class NavigationType {
+
+    @Id
+    private Long id;
+    private Long uid = 0L;
+    private String name;
+
+    private Long createDate = DateUtil.getTimestamp();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Long createDate) {
+        this.createDate = createDate;
+    }
+}
