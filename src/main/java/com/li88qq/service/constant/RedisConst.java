8049853e5489ef197e3f98eb1d337961673063e0
li88qq <573258011@qq.com>
2021-08-26 23:56:36 +0800

    增加redis常量配置.

---
+++ src/main/java/com/li88qq/service/constant/RedisConst.java
@@ -0,0 +1,29 @@
+package com.li88qq.service.constant;
+
+/**
+ * redis常量
+ *
+ * @author li88qq
+ * @version 1.0 2021/8/25 23:11
+ */
+public class RedisConst {
+
+    public static final String KEY_SESSION = "SESSION";//session key
+    public static final long EXPIRE_SESSION = 2;//session有效期,单位:小时
+
+    public static final String KEY_CAPTCHA = "CAPTCHA";//验证码key
+    public static final long EXPIRE_CAPTCHA = 3;//验证码有效期,单位:分钟
+
+    public static final String SEP = "-";
+
+    /**
+     * 构建key
+     *
+     * @param prefix 前缀
+     * @param key    key
+     * @return
+     */
+    public static String initKey(String prefix, String key) {
+        return String.join("", SEP, prefix, SEP, key);
+    }
+}
