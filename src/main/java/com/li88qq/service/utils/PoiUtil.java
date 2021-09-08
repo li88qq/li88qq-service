6b848b175334b4bb6f2e1b9b65df893f0294037e
li88qq <573258011@qq.com>
2021-09-08 23:19:54 +0800

    wip:poi工具类(开发中).

---
+++ src/main/java/com/li88qq/service/utils/PoiUtil.java
@@ -0,0 +1,19 @@
+package com.li88qq.service.utils;
+
+/**
+ * poi工具类
+ *
+ * @author li88qq
+ * @version 1.0 2021/9/7 23:26
+ */
+public class PoiUtil {
+
+    //doc http://poi.apache.org/components/spreadsheet/quick-guide.html
+
+    //生成备注
+    //合并单元格
+    //冻结单元格
+    //样式
+    //下拉框
+
+}
