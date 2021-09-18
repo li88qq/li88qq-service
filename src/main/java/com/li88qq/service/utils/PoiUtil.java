7026c9a818a8610561660569bbec99d06b4e78ae
li88qq <573258011@qq.com>
2021-09-18 23:42:34 +0800

    更新POI工具类方法.

--- src/main/java/com/li88qq/service/utils/PoiUtil.java
+++ src/main/java/com/li88qq/service/utils/PoiUtil.java
@@ -1,5 +1,8 @@
 package com.li88qq.service.utils;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellRangeAddress;
+
 /**
  * poi工具类
  *
@@ -11,8 +14,32 @@ public class PoiUtil {
     //doc http://poi.apache.org/components/spreadsheet/quick-guide.html
 
     //生成备注
-    //合并单元格
-    //冻结单元格
+
+    /**
+     * 合并单元格
+     *
+     * @param sheet    单元表
+     * @param firstRow 开始行 0开始
+     * @param lastRow  结束行,0开始
+     * @param firstCol 开始列,0开始
+     * @param lastCol  结束列,0开始
+     */
+    public static void merged(Sheet sheet, int firstRow, int lastRow, int firstCol, int lastCol) {
+        sheet.addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstCol, lastCol));
+    }
+
+    /**
+     * 冻结单元格
+     *
+     * @param sheet    单元表
+     * @param firstRow 开始行 0开始
+     * @param lastRow  结束行,0开始,不包括该行,
+     * @param firstCol 开始列,0开始
+     * @param lastCol  结束列,0开始,不包括该列
+     */
+    public static void freezePane(Sheet sheet, int firstRow, int lastRow, int firstCol, int lastCol) {
+        sheet.createFreezePane(firstRow, lastRow, firstCol, lastCol);
+    }
     //样式
     //下拉框
 
