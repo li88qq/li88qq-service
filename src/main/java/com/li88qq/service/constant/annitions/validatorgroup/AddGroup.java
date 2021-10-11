5616f174eb061e7ed895a8718d215f93513a00aa
li88qq <573258011@qq.com>
2021-10-11 23:19:36 +0800

    新增参数校验分组.

---
+++ src/main/java/com/li88qq/service/constant/annitions/validatorgroup/AddGroup.java
@@ -0,0 +1,10 @@
+package com.li88qq.service.constant.annitions.validatorgroup;
+
+/**
+ * 校验分组-新增
+ *
+ * @author li88qq
+ * @version 1.0 2021/10/11 23:17
+ */
+public interface AddGroup {
+}
---
+++ src/main/java/com/li88qq/service/constant/annitions/validatorgroup/UpdateGroup.java
@@ -0,0 +1,10 @@
+package com.li88qq.service.constant.annitions.validatorgroup;
+
+/**
+ * 校验分组-修改
+ *
+ * @author li88qq
+ * @version 1.0 2021/10/11 23:18
+ */
+public interface UpdateGroup {
+}
