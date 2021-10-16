774f3145b2707efed1520993844601b477a2bf51
li88qq <573258011@qq.com>
2021-10-16 19:17:23 +0800

    暂时去掉websocket.

--- src/main/java/com/li88qq/service/config/bean/WebSocketConfig.java
+++ src/main/java/com/li88qq/service/config/bean/WebSocketConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 /**
  * web
  */
-@Configuration
+//@Configuration
 public class WebSocketConfig {
 
     /**
      * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
      */
-    @Bean
+//    @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
