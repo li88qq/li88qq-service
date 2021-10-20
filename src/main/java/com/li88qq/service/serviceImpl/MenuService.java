76247e07e3bb81cc70e1950f11c93ae2479c9d1c
li88qq <573258011@qq.com>
2021-10-20 23:34:59 +0800

    增加获取菜单接口.

---
+++ src/main/java/com/li88qq/service/serviceImpl/MenuService.java
@@ -0,0 +1,27 @@
+package com.li88qq.service.serviceImpl;
+
+import com.li88qq.service.response.MenuVo;
+import com.li88qq.service.service.IMenuService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 菜单服务
+ *
+ * @author li88qq
+ * @version 1.0 2021/10/20 23:33
+ */
+@Service
+public class MenuService implements IMenuService {
+
+    /**
+     * 获取列表
+     *
+     * @return
+     */
+    @Override
+    public List<MenuVo> getList() {
+        return null;
+    }
+}
