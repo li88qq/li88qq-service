64c189df4bd626057759fb490040357691ff6b8e
li88qq <573258011@qq.com>
2021-11-20 23:34:13 +0800

    调整LogUtil工具类.

--- src/main/java/com/li88qq/service/utils/LogUtil.java
+++ src/main/java/com/li88qq/service/utils/LogUtil.java
@@ -18,48 +18,50 @@ public class LogUtil {
      * @return logger
      */
     public static Logger getLogger() {
-        return getLogger(2);
+        return getLogger(false);
     }
 
     /**
      * 公共方法
      *
-     * @param index 栈索引,区别:2-为直接返回logger,3-需要调用一次当前类(LogUtil)的当前方法(getLogger)
+     * @param inner 栈索引,区别:false-为直接返回logger,此时取栈底索引,true-需要调用一次当前类(LogUtil)的当前方法(getLogger)
      * @return logger
      */
-    private static Logger getLogger(int index) {
-        return LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[index].getClassName());
+    private static Logger getLogger(boolean inner) {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        int index = inner ? 3 : stackTrace.length - 1;
+        return LoggerFactory.getLogger(stackTrace[index].getClassName());
     }
 
     public static void debug(String msg) {
-        getLogger(3).debug(msg);
+        getLogger(true).debug(msg);
     }
 
     public static void debug(String msg, Throwable throwable) {
-        getLogger(3).debug(msg, throwable);
+        getLogger(true).debug(msg, throwable);
     }
 
     public static void info(String msg) {
-        getLogger(3).info(msg);
+        getLogger(true).info(msg);
     }
 
     public static void info(String msg, Throwable throwable) {
-        getLogger(3).info(msg, throwable);
+        getLogger(true).info(msg, throwable);
     }
 
     public static void warn(String msg) {
-        getLogger(3).warn(msg);
+        getLogger(true).warn(msg);
     }
 
     public static void warn(String msg, Throwable throwable) {
-        getLogger(3).warn(msg, throwable);
+        getLogger(true).warn(msg, throwable);
     }
 
     public static void error(String msg) {
-        getLogger(3).error(msg);
+        getLogger(true).error(msg);
     }
 
     public static void error(String msg, Throwable throwable) {
-        getLogger(3).error(msg, throwable);
+        getLogger(true).error(msg, throwable);
     }
 }
