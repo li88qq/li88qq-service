d135c4d77746d9973787fa60ab544efe4310beec
li88qq <573258011@qq.com>
2021-12-30 23:42:38 +0800

    [db]:新增工具类.

---
+++ db/src/main/java/com/li88qq/db/utils/BeanUtil.java
@@ -0,0 +1,10 @@
+package com.li88qq.db.utils;
+
+/**
+ * bean工具
+ *
+ * @author li88qq
+ * @version 1.0 2021/12/30 23:40
+ */
+public class BeanUtil {
+}
---
+++ db/src/main/java/com/li88qq/db/utils/SqlFactory.java
@@ -0,0 +1,10 @@
+package com.li88qq.db.utils;
+
+/**
+ * sql构建工厂
+ *
+ * @author li88qq
+ * @version 1.0 2021/12/30 23:40
+ */
+public class SqlFactory {
+}
