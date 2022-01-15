89d06f6af4aad38e03b82df956ae30558a28ca62
li88qq <573258011@qq.com>
2022-01-15 23:17:25 +0800

    [gateway]:初始化.

---
+++ gateway/src/main/java/com/li88qq/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.li88qq.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * gateway
+ *
+ * @author li88qq
+ * @version 1.0 2022/1/12 22:46
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
