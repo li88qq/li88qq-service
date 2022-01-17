ecff82fe905c253035f918727e9471340aa31bb1
li88qq <573258011@qq.com>
2022-01-17 23:39:02 +0800

    调整主pom.xml依赖.

---
+++ main/src/main/java/com/li88qq/main/MainApplication.java
@@ -0,0 +1,16 @@
+package com.li88qq.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author li88qq
+ * @version 1.0 2021/12/17 22:53
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
