e8a23b277617625dc7260aac16c67e41e225a225
li88qq <573258011@qq.com>
2022-04-03 23:11:50 +0800

    [util]:增加简单的正则工具.

---
+++ utils/src/main/java/com/li88qq/utils/RegexUtil.java
@@ -0,0 +1,37 @@
+package com.li88qq.utils;
+
+/**
+ * 正则工具类
+ *
+ * @author li88qq
+ * @version 1.0 2022/4/3 23:02
+ */
+public class RegexUtil {
+
+    /**
+     * 简单判断是否手机号码
+     *
+     * @param mobile 手机号码
+     * @return 是否手机号码
+     */
+    public static boolean isMobile(String mobile) {
+        if (mobile == null) {
+            return false;
+        }
+        return mobile.matches("1[0-9]{10}");
+    }
+
+    /**
+     * 手机号码打星号
+     *
+     * @param mobile 手机号码
+     * @return 手机号码
+     */
+    public static String markMobile(String mobile) {
+        if (!isMobile(mobile)) {
+            return mobile;
+        }
+        return mobile.substring(0, 3) + "****" + mobile.substring(7);
+    }
+
+}
