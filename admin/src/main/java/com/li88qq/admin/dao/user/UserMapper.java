be9e210365039d0a767ccfa2fcb2c2be60283909
li88qq <573258011@qq.com>
2022-04-04 23:20:21 +0800

    [admin]:增加用户分页查询功能.

---
+++ admin/src/main/java/com/li88qq/admin/dao/user/UserMapper.java
@@ -0,0 +1,34 @@
+package com.li88qq.admin.dao.user;
+
+import com.li88qq.admin.dto.user.UserPageForm;
+import com.li88qq.admin.dto.user.UserPageVo;
+import com.li88qq.db.annotion.Condition;
+import com.li88qq.db.annotion.Format;
+import com.li88qq.db.dto.Page;
+import com.li88qq.db.dto.Pageable;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * 用户管理
+ *
+ * @author li88qq
+ * @version 1.0 2022/4/4 23:16
+ */
+public interface UserMapper {
+
+    /**
+     * 分页查询用户列表
+     */
+    @Select("select * from User :where order by id desc")
+    @Condition("state = :form.state")
+    @Condition("loginDate >= :form.loginDateBegin")
+    @Condition("loginDate <= :form.loginDateEnd")
+    @Condition("createDate >= :form.createDateBegin")
+    @Condition("createDate <= :form.createDateEnd")
+    @Condition("username like :form.username")
+    @Condition("name like :form.name")
+    @Condition("mobile like :form.mobile")
+    @Condition("loginIp like :form.loginIp")
+    Page<UserPageVo> findPage(@Param("form") @Format UserPageForm form, Pageable pageable);
+}
