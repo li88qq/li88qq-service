d5f9b87f00958c508a5ea30247426aa9f430e9cc
li88qq <573258011@qq.com>
2022-04-17 22:31:15 +0800

    [bean]:增加公共的ids请求参数.

---
+++ bean/src/main/java/com/li88qq/bean/dto/IdsForm.java
@@ -0,0 +1,20 @@
+package com.li88qq.bean.dto;
+
+/**
+ * ids请求参数
+ *
+ * @author li88qq
+ * @version 1.0 2022/4/17 22:29
+ */
+public class IdsForm {
+
+    private String ids;
+
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+}
