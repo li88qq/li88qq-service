beb5803aa7164516eac27930e462c822ecd5521d
li88qq <573258011@qq.com>
2022-05-20 23:19:27 +0800

    [elasticjob]:加入spring starter.

---
+++ elasticjob/src/main/java/com/li88qq/elasticjob/JobApplication.java
@@ -0,0 +1,16 @@
+package com.li88qq.elasticjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author li88qq
+ * @version 1.0 2022/5/20 22:55
+ */
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
