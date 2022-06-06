60f56e4fbe45c3d824bca3b42be2c6bf2f267283
li88qq <573258011@qq.com>
2022-06-06 08:51:29 +0800

    [main]:添加mqtt同步发送.

---
+++ main/src/main/java/com/li88qq/main/mqtt/MqttFuture.java
@@ -0,0 +1,71 @@
+package com.li88qq.main.mqtt;
+
+import com.li88qq.utils.UUIDUtil;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * mqtt同步发送
+ *
+ * @author li88qq
+ * @version 1.0 2022/5/27 23:32
+ */
+public class MqttFuture extends CompletableFuture<String> {
+
+    private static final Map<String, MqttFuture> FUTURE_MAP = new ConcurrentHashMap<>();
+
+    private String id;//唯一id
+
+    private MqttFuture() {
+    }
+
+    /**
+     * 构建MqttFuture
+     *
+     * @return MqttFuture
+     */
+    public static MqttFuture build() {
+        MqttFuture future = new MqttFuture();
+        String uuid = UUIDUtil.uuid19();
+
+        FUTURE_MAP.put(uuid, future);
+
+        future.setId(uuid);
+        return future;
+    }
+
+    /**
+     * 完成
+     *
+     * @param id    消息id
+     * @param resul 响应结果
+     */
+    public static void done(String id, String resul) {
+        if (id != null) {
+            MqttFuture mqttFuture = FUTURE_MAP.remove(id);
+            if (mqttFuture != null && !mqttFuture.isDone()) {
+                mqttFuture.complete(resul);
+            }
+        }
+    }
+
+    /**
+     * 移除id
+     *
+     * @param id id
+     */
+    public static void remove(String id) {
+        if (id != null) {
+            FUTURE_MAP.remove(id);
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+}
