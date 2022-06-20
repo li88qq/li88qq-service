00a3060be099d463c18873029a07992cf7ee98bb
li88qq <573258011@qq.com>
2022-06-20 23:54:49 +0800

    [db]:新增MapperFactory.

---
+++ db/src/main/java/com/li88qq/db/core/MapperFactory.java
@@ -0,0 +1,16 @@
+package com.li88qq.db.core;
+
+/**
+ * mapper工厂
+ *
+ * @author li88qq
+ * @version 1.0 2022/6/20 23:35
+ */
+public interface MapperFactory {
+
+    //新增
+    //新增并返回id
+    //修改
+    //删除
+
+}
