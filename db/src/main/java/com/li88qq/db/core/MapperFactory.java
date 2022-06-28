80ff43def986875e8b53a666ee30888bb0842770
li88qq <573258011@qq.com>
2022-06-28 23:39:45 +0800

    [db]:增加默认mapper模板.

--- db/src/main/java/com/li88qq/db/core/MapperFactory.java
+++ db/src/main/java/com/li88qq/db/core/MapperFactory.java
@@ -1,5 +1,7 @@
 package com.li88qq.db.core;
 
+import org.apache.ibatis.annotations.*;
+
 /**
  * mapper工厂
  *
@@ -8,9 +10,45 @@ package com.li88qq.db.core;
  */
 public interface MapperFactory {
 
-    //新增
-    //新增并返回id
-    //修改
-    //删除
+    /**
+     * 保存实体
+     *
+     * @param t   实体
+     * @param sql insert语句
+     * @return 影响行数
+     */
+    @Insert("${sql}")
+    <T> int save(@Param("t") T t, @Param("sql") String sql);
 
+    /**
+     * 保存实体并返回自增主键
+     *
+     * @param t   实体对象
+     * @param sql insert语句
+     * @param id  自增主键
+     * @return 影响行数, 返回的实体对象中会自动赋值主键
+     */
+    @Insert("${sql}")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    <T> int saveToId(@Param("t") T t, @Param("sql") String sql, @Param("id") String id);
+
+    /**
+     * 保存实体
+     *
+     * @param t   实体
+     * @param sql update语句
+     * @return 影响行数
+     */
+    @Update("${sql}")
+    <T> int update(@Param("t") T t, @Param("sql") String sql);
+
+    /**
+     * 根据主键删除实体
+     *
+     * @param t   实体
+     * @param sql delete语句
+     * @return 影响行数
+     */
+    @Delete("${sql}")
+    <T> int delete(@Param("t") T t, @Param("sql") String sql);
 }
