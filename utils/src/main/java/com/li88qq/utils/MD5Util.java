f11fb1186a0f0816780451ce509d7e249763ac3b
li88qq <573258011@qq.com>
2022-07-26 23:58:57 +0800

    [util]:增加MD5工具类.

---
+++ utils/src/main/java/com/li88qq/utils/MD5Util.java
@@ -0,0 +1,18 @@
+package com.li88qq.utils;
+
+/**
+ * md5工具类
+ *
+ * @author li88qq
+ * @version 1.0 2022/7/26 23:18
+ */
+public class MD5Util {
+
+    public static String md5(String data) {
+        return null;
+    }
+
+    public static String byteToHex(byte[] bytes) {
+        return null;
+    }
+}
