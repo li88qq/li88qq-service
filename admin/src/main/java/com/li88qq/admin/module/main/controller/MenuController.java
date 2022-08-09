94f0978ffea0d9ffe87344c79a07b4c0c21d483c
li88qq <573258011@qq.com>
2022-08-09 23:35:56 +0800

    [admin]:初始化菜单和角色管理.

---
+++ admin/src/main/java/com/li88qq/admin/module/main/controller/MenuController.java
@@ -0,0 +1,22 @@
+package com.li88qq.admin.module.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 前台菜单管理
+ *
+ * @author li88qq
+ * @version 1.0 2022/8/9 23:32
+ */
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+    //新增
+    //修改
+    //分页查询
+    //查询详情
+    //排序
+
+}
---
+++ admin/src/main/java/com/li88qq/admin/module/main/controller/RoleController.java
@@ -0,0 +1,15 @@
+package com.li88qq.admin.module.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 角色管理
+ *
+ * @author li88qq
+ * @version 1.0 2022/8/9 23:35
+ */
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+}
