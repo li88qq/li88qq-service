6a0ebfec1ec3565beeb7b4a3fd4624444282d022
li88qq <573258011@qq.com>
2022-10-24 23:17:45 +0800

    [api]:增加独立的api文档项目.

---
+++ api/src/main/java/com/li88qq/ApiController.java
@@ -0,0 +1,18 @@
+package com.li88qq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author li88qq
+ * @version 1.0 2022/10/24 23:07
+ */
+@SpringBootApplication
+public class ApiController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiController.class, args);
+    }
+}
