b52dee36131ee191de55669976c689689d501df8
li88qq <573258011@qq.com>
2022-11-24 23:00:22 +0800

    [db]:调整@Ds,@Db注解使用范围.

--- db/src/main/java/com/li88qq/db/annotion/Db.java
+++ db/src/main/java/com/li88qq/db/annotion/Db.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * @author li88qq
  * @version 1.0 2022/6/24 23:15
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Db {
 
