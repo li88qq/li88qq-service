6f9f97daacabd0e9b15587d05a67fe1552a9c6c9
li88qq <573258011@qq.com>
2023-02-14 23:29:59 +0800

    [db]:调整条件枚举类

---
+++ db/src/main/java/com/li88qq/db/enums/If.java
@@ -0,0 +1,17 @@
+package com.li88qq.db.enums;
+
+/**
+ * 条件处理
+ *
+ * @author li88qq
+ * @version 1.0 2023/2/14 23:17
+ */
+public enum If {
+
+    NONE,//不处理
+    NOT_NULL,//非空,如果是字符串,包括非空字符
+
+    GT_EQ,//大于等于0
+    GT,//大于0
+
+}
---
+++ db/src/main/java/com/li88qq/db/enums/Join.java
@@ -0,0 +1,13 @@
+package com.li88qq.db.enums;
+
+/**
+ * 连接符
+ *
+ * @author li88qq
+ * @version 1.0 2023/2/14 23:27
+ */
+public enum Join {
+
+    AND,
+    OR,
+}
