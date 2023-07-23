fc791864e6f2d8a80e0223b04115a14690c164d4
li88qq <573258011@qq.com>
2023-07-23 19:19:19 +0800

    [bean]:增加AppConfig

--- bean/src/main/resources/main.sql
+++ bean/src/main/resources/main.sql
@@ -11,3 +11,21 @@ create table Menu(
     updateDate bigint not null default 0 comment '更新时间',
     primary key(id)
 ) comment '菜单表';
+
+create table AppConfig(
+    id int not null auto_increment comment '自增主键',
+    domain varchar(32) comment '域名',
+    appName varchar(32) comment '应用名称',
+    ico varchar(100) comment '小图标',
+    logo varchar(100) comment 'logo',
+    beiAnIco varchar(100) comment '备案图标',
+    beiAn varchar(100) comment '备案',
+    beiAnLink varchar(100) comment '备案链接',
+    icp varchar(100) comment 'icp',
+    icpLink varchar(100) comment 'icp链接',
+    copyRight varchar(100) comment '版权声明',
+    createDate bigint not null default 0 comment '创建时间',
+    updateDate bigint not null default 0 comment '更新时间',
+    primary key(id),
+    unique (domain)
+) comment '应用配置表';
