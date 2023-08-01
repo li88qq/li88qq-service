f8d66e759a4e75a07254963a71465aeada874f8b
li88qq <573258011@qq.com>
2023-08-01 21:47:31 +0800

    [login]:初始化项目

--- login/src/main/java/com/li88qq/login/LoginApplication.java
+++ login/src/main/java/com/li88qq/login/LoginApplication.java
@@ -1,8 +1,18 @@
 package com.li88qq.login;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
+ * 登录服务
+ *
  * @author li88qq
  * @version 1.0 2023/7/21 22:41
  */
+@SpringBootApplication
 public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
 }
---
+++ login/src/main/java/com/li88qq/login/auth/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 授权模块
+ *
+ * @author li88qq
+ * @version 1.0 2023/7/31 23:36
+ */
+package com.li88qq.login.auth;
