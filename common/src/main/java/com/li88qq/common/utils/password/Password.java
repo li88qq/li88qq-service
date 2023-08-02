c0f1b263ba2af19e30686710db5987051d5f0b78
li88qq <573258011@qq.com>
2023-08-02 22:39:12 +0800

    [common]:加入密码加密工具

---
+++ common/src/main/java/com/li88qq/common/utils/password/Password.java
@@ -0,0 +1,29 @@
+package com.li88qq.common.utils.password;
+
+/**
+ * 密码对象
+ *
+ * @author li88qq
+ * @version 1.0 2022/3/20 22:14
+ */
+public class Password {
+
+    private String password;
+    private String salt;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
