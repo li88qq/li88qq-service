827890e88d62987c9138b5328b224b76946b7923
li88qq <573258011@qq.com>
2023-10-25 22:16:46 +0800

    [main]:完善记录查询接口

--- main/src/main/java/com/li88qq/main/service/impl/LogServiceImpl.java
+++ main/src/main/java/com/li88qq/main/service/impl/LogServiceImpl.java
@@ -1,11 +1,17 @@
 package com.li88qq.main.service.impl;
 
+import com.li88qq.db.dto.page.Page;
+import com.li88qq.db.dto.page.Pageable;
 import com.li88qq.db.dto.page.TPage;
+import com.li88qq.main.dao.log.ActionLogMapper;
+import com.li88qq.main.dao.log.LoginLogMapper;
 import com.li88qq.main.dto.log.GetActionLogPageForm;
 import com.li88qq.main.dto.log.GetActionLogPageVo;
 import com.li88qq.main.dto.log.GetLoginLogPageForm;
 import com.li88qq.main.dto.log.GetLoginLoginPageVo;
 import com.li88qq.main.service.LogService;
+import com.li88qq.main.util.SessionUtil;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,12 +23,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class LogServiceImpl implements LogService {
 
+    @Resource
+    private LoginLogMapper loginLogMapper;
+    @Resource
+    private ActionLogMapper actionLogMapper;
+
     /**
      * 分页查询登录记录
      */
     @Override
     public TPage<GetLoginLoginPageVo> getLoginPage(GetLoginLogPageForm form) {
-        return null;
+        Integer uid = SessionUtil.getUid();
+        Pageable pageable = new Pageable(form.getPage(), form.getPageSize());
+        Page<GetLoginLoginPageVo> voPage = loginLogMapper.getPage(uid, form, pageable);
+        return voPage.build();
     }
 
     /**
@@ -30,6 +44,9 @@ public class LogServiceImpl implements LogService {
      */
     @Override
     public TPage<GetActionLogPageVo> getActionPage(GetActionLogPageForm form) {
-        return null;
+        Integer uid = SessionUtil.getUid();
+        Pageable pageable = new Pageable(form.getPage(), form.getPageSize());
+        Page<GetActionLogPageVo> voPage = actionLogMapper.getPage(uid, form, pageable);
+        return voPage.build();
     }
 }
